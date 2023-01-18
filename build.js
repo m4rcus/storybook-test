f529242e43e91fa85d4d17123d3d0ee14cfa8e82
m4rcus <marcuspatock@gmail.com>
2023-01-19 00:06:39 +0100

    ci

---
+++ build.js
@@ -0,0 +1,43 @@
+const StyleDictionaryPackage = require('style-dictionary');
+
+// HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
+
+function getStyleDictionaryConfig(brand, platform) {
+  return {
+    "source": [
+      `tokens/${brand}.json`
+    ],
+    "platforms": {
+      "css": {
+        "transformGroup": "css",
+        "buildPath": "css/",
+        "files": [{
+          "destination": `${brand}.css`,
+          "format": "css/variables"
+        }]
+      },
+    }
+  };
+}
+
+console.log('Build started...');
+
+// PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
+
+['input', 'bf', 'mw'].map(function (brand) {
+  ['css'].map(function (platform) {
+
+    console.log('\n==============================================');
+    console.log(`\nProcessing: [${platform}] [${brand}]`);
+
+    const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(brand, platform));
+
+    StyleDictionary.buildPlatform(platform);
+
+    console.log('\nEnd processing');
+
+  })
+})
+
+console.log('\n==============================================');
+console.log('\nBuild completed!');
